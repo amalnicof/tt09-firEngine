bd3ec0e778969ae0d59027bf4bf449daae83be9e
ArandomDev <haow6449@gmail.com>
2024-10-21 16:04:19 +0000

    Migrated RTL and started tb

---
+++ src/project.sv
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  wire mclk;
+  wire lrck;
+  wire sclk;
+  wire dac;
+  wire adc = uio_in[3];
+
+  wire cs = ui_in[0];
+  wire mosi = ui_in[1];
+  wire spiClk = ui_in[3];
+
+  assign uio_out[0] = mclk;
+  assign uio_out[4] = mclk;
+  assign uio_out[1] = lrck;
+  assign uio_out[5] = lrck;
+  assign uio_out[2] = sclk;
+  assign uio_out[6] = sclk;
+  assign uio_out[7] = dac;
+
+  assign uio_oe = 8'b11110111;
+  assign uo_out = 8'b0;
+
+  FIREngine #(
+      .NTaps(9)
+  ) firEngine (
+      .clk(clk),
+      .resetN(rst_n),
+
+      // I2S2 Port
+      .mclk(mclk),
+      .sclk(sclk),
+      .lrck(lrck),
+      .adc (adc),
+      .dac (dac),
+
+      // SPI Port
+      .spiClk(spiClk),
+      .mosi(mosi),
+      .cs(cs)
+  );
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, uio_in[2:0], uio_in[7:4], ui_in[2], ui_in[7:4], uio_out[3], 1'b0};
+
+endmodule
--- test/test.py
+++ test/test.py
@@ -3,16 +3,55 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, FallingEdge, RisingEdge
+from cocotb.handle import SimHandleBase
+
+async def resetCore(dut: SimHandleBase) -> None:
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 2)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 2)
+
+class SPIModel(object):
+    def __init__(self, dut: SimHandleBase) -> None:
+        self.sclk = dut.ui_in[3]
+        self.mosi = dut.ui_in[1]
+        self.cs = dut.ui_in[0]
+
+        # 1MHz clock
+        Clock(self.sclk, 500, units="ns")
+
+    def sendData(self, data: bytes, dropTail: int) -> None:
+        """Send data to dut.
+        starts from LSB of first byte.
+        """
+
+        RisingEdge(self.sclk)
+        self.cs.value = 0
+
+        for i, byte in enumerate(data):
+            if i == len(data)-1:
+                stop = dropTail
+            else:
+                stop = 8
+
+            for j in range(stop):
+                FallingEdge(self.sclk)
+                # self
+        pass
+    pass
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+async def test_project(dut: SimHandleBase):
+    # Set the clock period to 20ns 50MHz
+    clock = Clock(dut.clk, 20, units="ns")
     cocotb.start_soon(clock.start())
+
+
+
+    await resetCore(dut)
+    return
 
     # Reset
     dut._log.info("Reset")
